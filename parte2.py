411c1a0899fa99a869156335ac475c46c086b233
fcp16 <fcp16@inf.ufpr.br>
2021-02-11 10:18:06 -0300

    pratica1 pronta

--- parte2.py
+++ parte2.py
@@ -1,59 +1,35 @@
-tcp = open('tcp.txt','r')
-    # alert tcp $EXTERNAL_NET any -> $HOME_NET 21 (msg:"PROTOCOL-FTP ADMw0rm ftp login attempt";
-    # flow:to_server,established; 
-    # content:"USER"; 
-    # nocase; 
-    # content:"w0rm"; 
-    # distance:1; 
-    # nocase; 
-    # pcre:"/^USER\s+w0rm/smi"; 
-    # metadata:ruleset community, service ftp; 
-    # classtype:suspicious-login; 
-    # sid:144;
-    # rev:16;)
+import re  
 
-    ## alert tcp $EXTERNAL_NET any -> $HOME_NET 53 (msg:"OS-OTHER x86 FreeBSD overflow attempt"; 
-    # flow:to_server,established; 
-    # content:"|EB|n^|C6 06 9A|1|C9 89|N|01 C6|F|05|"; 
-    # metadata:ruleset community, service dns; 
-    # classtype:attempted-admin; 
-    # sid:266;
-    # rev:15;)
+file1 = open('xablau.txt','r')
+file3 = open('parte2.txt','w')
 
-    for line in tcp:
-
-udp = open('udp.txt','r')
-ip = open('ip.txt','r')
-icmp = open('icmp.txt','r')
-
-file2 = open('sid-msg.map','r')
-
-    dic = {}
-    for line in f:
-        if 
-  
-    with open('parte1_a.txt','w') as f2:
-        for chave in lista_chaves:
-            f2.write(chave + '-' + str(dic[chave])+'\n')
- 
-
-
-#0 alert tcp $HOME_NET 2589 -> $EXTERNAL_NET any (msg:"MALWARE-BACKDOOR - Dagger_1.4.0"
-#1 flow:to_client,established
-#2 content:"2|00 00 00 06 00 00 00|Drives|24 00|"
-#3 depth:16
-#4 metadata:ruleset community
-#5 classtype:misc-activity
-#6 sid:105
-#7 rev:14
-#8 )
-
-#0 alert tcp $HOME_NET 15104 -> $EXTERNAL_NET any (msg:"MALWARE-OTHER mstream handler to client"
-#1  flow:to_client,established
-#2 content:">"
-#3 metadata:ruleset community
-#4 reference:cve,2000-0138
-#5 classtype:attempted-dos
-#6 sid:250
-#7 rev:10
-# )
+for line in file1:
+    prot = re.search(r'^.*alert ([a-z]+).*',line).group(1)
+    port = re.search(r'^.+ ([0-9|a-z|A-Z|\_|\$|\:|[\[|\]|\,]+) \(',line).group(1)
+    sid = re.search(r'^.+sid:([0-9]+).*',line).group(1)
+    # file3.writelines('resultado: protocolo:'+ prot + ' - porta:'+ port + ' - sid:' + sid)
+    sid_def = None
+    file2 = open('sid-msg.map','r')
+    for line2 in file2:
+        # print 'sid->' + str(sid)
+        # print 'line split->' + str(line2.split(' || ')[0])
+        if str(sid) == str(line2.split(' || ')[0]):
+            sid_def = str(line2.split(' || ')[1])
+            sid_def = sid_def.replace("\n", "")
+            # print 'sid def->'+ sid_def
+            break
+            # print str(sid) + '==' +  str(line2.split('||')[0])
+    file2.close()
+    
+    # print string1 + '-' + str(sid_def)
+    if prot == 'tcp':
+        file3.write('1,'+ port +','+ str(sid_def))
+    if prot == 'udp':
+        file3.write('2,'+ port +','+ str(sid_def))
+    if prot == 'ip':
+        file3.write('0,'+ port +','+ str(sid_def))
+    if prot == 'icmp':
+        file3.write('3,'+ port +','+ str(sid_def))
+    file3.write('\n')
+file1.close()
+file3.close()
